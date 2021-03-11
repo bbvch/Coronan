afec2268695c1572088c103180294a3cac57e337
meshell <estermann.michel@gmail.com>
2021-03-11 23:25:20 +0000

    Added cmake-preset support in build.sh.

--- build.sh
+++ build.sh
@@ -2,54 +2,53 @@
 
 set -e
 
-BUILD_DIR=""
+BUILD_DIR="build"
 CMAKE=cmake
 COVERAGE=false
 COVERAGE_OUT=""
 BUILD_TYPE=Debug
 BUILD_TARGET=""
-
-
+NINJA=""
+COMPILER="g++"
 
 print_usage() {
 cat << EOM
-Usage: build.sh [options] build_dir
+Usage: build.sh [options] [build_dir]
   Available options:
     -h|--help          Print this help
-    --cov output_file  Build debug version with coverage enabled.
-    -r|--release       Build release version. Note: is ignored when --cov is enabled
-    --cmake  path      Path to cmake (default is the system cmake)
+    --cov=output_file  Build debug version with coverage enabled.
+    -r|--release       Build release version. 
+                       Note: is ignored when --cov is set
+    --cmake=path       Path to cmake (default is the system cmake)
     -i                 Install
     -p                 Create package
+    --clang=version    Build with clang (ver: version).
+                       Note: is ignored when --cov is set
+   build_dir:      Directory to build in (default: build)
 EOM
 }
-
-if [ "$#" -lt 1 ]; then
-    echo "Error: Please specify the build folder."
-    echo ""
-    print_usage
-    exit 1
-fi
 
 if [ $# -ge 1 ]; then
     while [[ $# -gt 0 ]] ; do
         key="$1"
 
         case "$key" in
-        --cov)
+        --cov=*)
             COVERAGE=true
-            COVERAGE_OUT="$2"
-            shift # past argument
-            shift # past value
+            COVERAGE_OUT="${key#*=}"
+            shift # past argument=value
             ;;
         -r|--release)
             BUILD_TYPE=Release
             shift # past argument
             ;;
-        --cmake)
-            CMAKE="$2"
-            shift # past argument
-            shift # past value
+        --clang=*)
+            COMPILER="clang++-${key#*=}"
+            shift # past argument=value
+            ;;
+        --cmake=*)
+            CMAKE="${key#*=}"
+            shift # past argument=value
             ;;
         -h|--help)
             print_usage
@@ -64,31 +63,42 @@ if [ $# -ge 1 ]; then
             shift # past argument
             ;;
         *)
-            BUILD_DIR=$1
-            shift # past argument
+            BUILD_DIR=$key
+            shift # past argument=value
             ;;
         esac
     done
 fi
 
-if [ -z "${BUILD_DIR}" ]; then
-    echo "Error: Please specify the build folder."
+if [ "$COVERAGE" = true -a -z "${COVERAGE_OUT}" ]; then
+    echo "Error: Please specify the coverage output_file."
     echo ""
     print_usage
     exit 1
 fi
 
-[[ -d "${BUILD_DIR}" ]] || mkdir "${BUILD_DIR}"
-
-if command -v ninja &> /dev/null ]
+if [ command -v ninja &> /dev/null ]
 then
+    NINJA="-ninja"
     CMAKE_GENERATOR="-G Ninja"
 fi
 
-if [ "$COVERAGE" = true ] ; then
-    (cd "${BUILD_DIR}" && "${CMAKE}" $CMAKE_GENERATOR -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..)
-else
-    (cd "${BUILD_DIR}" && "${CMAKE}" $CMAKE_GENERATOR -DCODE_COVERAGE=OFF -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" ..)
+required_cmake_version="3.19.0"
+cmake_version=$("${CMAKE}" --version | head -n1 | cut -d" " -f3)
+
+if [ "$(printf '%s\n' "$required_cmake_version" "$cmake_version" | sort -V | head -n1)" = "$required_cmake_version" ]; then 
+    # Use CMake presets starting from cmake 3.19.0
+    if [ "$COVERAGE" = true ] ; then
+        "${CMAKE}" -S . --preset=linux-coverage"${NINJA}"
+    else
+        "${CMAKE}" -S . --preset=linux"${NINJA}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_CXX_COMPILER="${COMPILER}"
+    fi
+else   
+    if [ "$COVERAGE" = true ] ; then
+        "${CMAKE}" -S . -B ${BUILD_DIR} $CMAKE_GENERATOR -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug
+    else
+        "${CMAKE}" -S . -B ${BUILD_DIR} $CMAKE_GENERATOR -DCMAKE_CXX_COMPILER="${COMPILER}" -DCODE_COVERAGE=OFF -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
+    fi
 fi
 
 num_threads=$(grep -c '^processor' /proc/cpuinfo)
@@ -99,5 +109,6 @@ if [ "$COVERAGE" = true ] ; then
     lcov --capture --directory . --output-file "${COVERAGE_OUT}"
     lcov --remove "${COVERAGE_OUT}" '/usr/*' --output-file "${COVERAGE_OUT}"
 fi
+
 
 exit 0
