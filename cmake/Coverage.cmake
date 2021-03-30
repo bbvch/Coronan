9f6b1df2d1b227a0d8902f18afdfc8755cdb6897
Michel Estermann <estermann.michel@gmail.com>
2021-03-30 18:08:36 +0200

    Fix Coverage build in build.sh.

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -3,7 +3,7 @@ function(enable_coverage project_name)
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES
                                              ".*Clang"
   )
-    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" ON)
+    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" OFF)
 
     if(ENABLE_COVERAGE)
       target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
