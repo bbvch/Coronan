605aa0a3550cda810057a3b0306e4e00efe1ce8d
Michel Estermann <estermann.michel@gmail.com>
2025-03-12 17:20:40 +0000

    use fprofile-update=prefer-atomic) for coverage (mulithreaded code)

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -6,7 +6,10 @@ function(enable_coverage project_name)
         option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" OFF)
 
         if(ENABLE_COVERAGE)
-            target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
+            target_compile_options(
+                ${project_name} INTERFACE --coverage -O0 -g
+                                          -fprofile-update=prefer-atomic
+            )
             target_link_libraries(${project_name} INTERFACE --coverage)
         endif()
     endif()
