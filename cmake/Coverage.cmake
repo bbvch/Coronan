a508b059b164622abd1e5347b12595f550bf1889
Michel Estermann <estermann.michel@gmail.com>
2025-03-12 23:33:54 +0100

    optimize code coverage

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -7,8 +7,9 @@ function(enable_coverage project_name)
 
         if(ENABLE_COVERAGE)
             target_compile_options(
-                ${project_name} INTERFACE --coverage -O0 -g
-                                          -fprofile-update=prefer-atomic
+                ${project_name}
+                INTERFACE --coverage -O0 -g -fno-inline -fno-elide-constructors
+                          -fprofile-update=prefer-atomic
             )
             target_link_libraries(${project_name} INTERFACE --coverage)
         endif()
